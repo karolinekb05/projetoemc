6637fe7454ec5a6ee56c4e35bc66f3e1b5ad536b
karolinekb05 <79204560+karolinekb05@users.noreply.github.com>
2021-06-14 18:36:08 -0300

    Update mercado.c

--- mercado.c
+++ mercado.c
@@ -1,13 +1,3 @@
-/*
-Em um supermercado apresenta uma promoção aos clientes,
-o cliente que vir a totalizar em suas compras acima de R$ 300,00,
-o mesmo terá o desconto de 10% no valor que exceder a R$ 300,00.
-No final exibir os seguintes resultados :
-Total da Compra :
-Valor do Desconto :
-Valor a Pagar :
-*/
-
 #include<stdlib.h>
 #include<stdio.h>
 
